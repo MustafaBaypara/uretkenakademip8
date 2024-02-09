031f1b7b92b343055ebb84dae38a092305ac9003
Mustafa Baypara <94145699+MustafaBaypara@users.noreply.github.com>
2024-02-09 20:38:48 +0300

    a

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import TotalCompletedItems from "./components/TotalCompletedItems";
+
+function App() {
+  return (
+    <div className="App w-full h-screen bg-[#111111] text-white">
+      <h1 className="text-3xl font-bold p-6">Yapılacaklar Listesi</h1>
+      <AddTodo />
+      <TodoList />
+      <TotalCompletedItems />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TotalCompletedItems.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { clearAll } from "../redux/todoSlice";
+
+const TotalCompletedItems = () => {
+  const dispatch = useDispatch();
+  const completedTasks = useSelector((state) =>
+    state.todos.filter((task) => task.status === true)
+  );
+
+  const removeAll = () => {
+    dispatch(clearAll());
+  };
+
+  return (
+    <div>
+      <h4 className="mt-10 text-lg">
+        Yapılanların sayısı: {completedTasks.length}
+      </h4>
+      <button
+        onClick={removeAll}
+        className="bg-orange-500 py-1 px-2 tracking-wider mt-10 rounded-sm"
+      >
+        Temizle
+      </button>
+    </div>
+  );
+};
+
+export default TotalCompletedItems;
